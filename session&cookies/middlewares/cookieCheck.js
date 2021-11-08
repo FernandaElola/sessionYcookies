332645da08cd1867a322545ff5519ebcab326b88
FernandaElola <elolafernanda@gmail.com>
2021-11-08 16:01:15 -0300

    arreglo cookies

--- session&cookies/middlewares/cookieCheck.js
+++ session&cookies/middlewares/cookieCheck.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if(req.cookies.bgColor){
-        req.cookies.bgColor = req.session.bgColor
+        req.session.bgColor = req.cookies.bgColor
     }
     next()
 }
